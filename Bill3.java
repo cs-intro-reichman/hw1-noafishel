f1f23a9aa39c031a9fe3d22495bac848c786f3fa
Noa Fishel <noa.fishel@post.runi.ac.il>
2025-11-05 13:37:21 +0200

    completed assignment

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
 	    String name3 = args[2];
 		double totalBill = Double.parseDouble(args[3]);
 		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +
-		 					": pay " + Math.ceil(totalBill / 3) + " shekels each.");
+		 					": pay " + Math.ceil(totalBill / 3) + " shekels each");
 	}
 }
